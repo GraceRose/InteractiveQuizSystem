5a65c81094294699c955ffa69944804458f52bf6
GraceRose <102783279@R21100308.boiseschools.org>
2015-02-18 13:09:52 -0700

    Updated how questions are asked and how the class functions

--- InteractiveQuizSystem/Main.java
+++ InteractiveQuizSystem/Main.java
@@ -7,31 +7,49 @@ import java.util.*;
 public class Main{
     protected static int score;
     protected static String name;
-    
+    protected static Question test = new Question("What is the capital of Idaho?","Boise", "wikipedia.org");
     public static void main(String[] args){
         Scanner kbReader = new Scanner(System.in);
+        boolean x = true;
         String input = "";
         score = 0;
         System.out.println("Hello and welcome to the Interactive Quiz System. I am Cora, your guide through this process. Please type your name.");
         input = kbReader.nextLine();
         name = input;
-        System.out.println("Welcome to the IQS, " + name + "!");
-        
+        System.out.println("Welcome to the IQS, " + name + "!" + "\nWhat would you like to do?");
+        while(x){
+            input = kbReader.nextLine();
+            System.out.println("Your score is " + score);
+            System.out.println("What would you like to do?");
+            if(input.equalsIgnoreCase("exit")){
+                x = false;
+            }
+            else if(input.equalsIgnoreCase("test")){
+                askQuestion(test);
+                input = kbReader.nextLine();
+                checkAnswer(input, test);
+            }
+           
+        }
     }
     public static void addScore(int p){
         score += p;
     }
-    public static void checkAnswer(String input, Question quest){
+    public static void askQuestion(Question quest){
+        System.out.println(quest.getQuestion());
+    }
+    public static void checkAnswer(String i, Question quest){
         Scanner kbReader = new Scanner(System.in);
-        if(input.equalsIgnoreCase(quest.getAnswer())){
+        if(i.equalsIgnoreCase(quest.getAnswer())){
             quest.setCorrect(true);
             addScore(2);
             System.out.println("That was correct! Would you like more information on this topic?");
-            input = kbReader.nextLine();
+            String input = kbReader.nextLine();
             if(input.equalsIgnoreCase("yes")){
                 System.out.println("Here is the link to the Wikipedia article with more information:");
                 System.out.println(quest.getSource());
             }
+            
         }
         else{
             System.out.println("I'm sorry, that was not correct. Here is the link to the Wikipedia article with more information:");
