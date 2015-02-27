f485734639690b5a6b9a07dca561896d1d70e134
GraceRose <102783279@R21100311.boiseschools.org>
2015-02-27 13:10:03 -0700

    Added and completed the tester and checkAnswer methods in addition to completing the DevJournal for the week.

--- InteractiveQuizSystem/Main.java
+++ InteractiveQuizSystem/Main.java
@@ -7,7 +7,11 @@ import java.util.*;
 public class Main{
     protected static int score;
     protected static String name;
-    protected static Question test = new Question("What is the capital of Idaho?","Boise", "wikipedia.org", 2);
+    protected static Question test1 = new Question("What is the capital of Idaho?","Boise", "wikipedia.org", 2);
+    protected static Question test2 = new Question("In which state is the highest point in the United States?","Alaska","wikipedia.org",2);
+    protected static Question test3 = new Question("Which state contains the Great Salt Lake?","Utah","wikipedia.org",2);
+    protected static Question test4 = new Question("The only non-rectangular shaped flag belongs to what country?","Nepal","wikipedia.org",2);
+    protected static Question test5 = new Question("What state is the FRC team 254 from?","California","wikipedia.org",2);
     public static void main(String[] args){
         Scanner kbReader = new Scanner(System.in);
         boolean x = true;
@@ -16,29 +20,53 @@ public class Main{
         System.out.println("Hello and welcome to the Interactive Quiz System. I am Cora, your guide through this process. Please type your name.");
         input = kbReader.nextLine();
         name = input;
-        System.out.println("Welcome to the IQS, " + name + "!" + "\nWhat would you like to do?");
+        System.out.println("Welcome to the IQS, " + name + "! Press enter to start.");
         while(x){
             input = kbReader.nextLine();
+            System.out.println("Your score is " + score);
             System.out.println("What would you like to do?");
             if(input.equalsIgnoreCase("exit")){
+                System.out.println("Oh right you want to leave. Fine.");
                 x = false;
             }
-            System.out.println("Your score is " + score);
-
+            
             if(input.equalsIgnoreCase("fact")){
                 System.out.println("This statement is false");
             }
             if(input.equalsIgnoreCase("create")){
-                
+
             }
 
             if(input.equalsIgnoreCase("test")){
+                /*
                 askQuestion(test);
                 input = kbReader.nextLine();
                 checkAnswer(input, test);
+                */
+               test();
             }
 
         }
+    }
+    
+    public static void test(){
+        Scanner kbReader = new Scanner(System.in);
+        askQuestion(test1);
+        String input = kbReader.nextLine();
+        checkAnswer(input,test1);
+        askQuestion(test2);
+        input = kbReader.nextLine();
+        checkAnswer(input,test2);
+        askQuestion(test3);
+        input = kbReader.nextLine();
+        checkAnswer(input,test3);
+        askQuestion(test4);
+        input = kbReader.nextLine();
+        checkAnswer(input,test4);
+        askQuestion(test5);
+        input = kbReader.nextLine();
+        checkAnswer(input,test5);
+        
     }
 
     public static void addScore(int p){
@@ -59,6 +87,7 @@ public class Main{
             if(input.equalsIgnoreCase("yes")){
                 System.out.println("Here is the link to the Wikipedia article with more information:");
                 System.out.println(quest.getSource());
+                System.out.println("Press enter to move on to the next question.");
 
             }
 
